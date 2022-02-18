19463d71ae93ebc120f41af7c9f787322f4971f2
Ly Nguyen <contact@minhlynguyenvo.com>
2022-02-18 09:34:09 +0100

    fix: export date picker component

--- src/Input/index.ts
+++ src/Input/index.ts
@@ -2,3 +2,4 @@ export {TextInput} from "./TextInput";
 export {Selector} from "./Selector";
 export {Checkbox} from "./Checkbox";
 export {DateTimePicker} from "./DateTimePicker";
+export {DatePicker} from "./DatePicker";
