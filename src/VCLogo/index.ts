a48848da4dd24a5c865ff8832bbc23333cb6b73c
Ly Nguyen <contact@minhlynguyenvo.com>
2022-05-05 10:30:13 +0200

    chore: try to export svg as react element

--- src/VCLogo/index.ts
+++
@@ -1,2 +0,0 @@
-export {default as VCLogo} from "./VC_LOGO_RGB_Main.svg";
-export {default as VCLogoIcon} from "./VC_SIGNE_RGB_Main.svg";
---
+++ src/VCLogo/index.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+import {default as VCLogoSvg} from "./VC_LOGO_RGB_Main.svg";
+import {default as VCLogoSigneSvg} from "./VC_SIGNE_RGB_Main.svg";
+
+export const VCLogo = () => (<img src={VCLogoSvg}  alt="Variocube GmbH" />)
+export const VCLogoIcon = () => (<img src={VCLogoSigneSvg}  alt="Variocube GmbH" />)
