9ab5e0de7112ecbacd613286b171c4d4bec79813
Peter Karrer <peter@karrer.pro>
2022-06-28 23:46:58 +0200

    chore: remove old date/time pickers chore: upgrade deps chore: version

--- src/date-pickers/index.ts
+++ src/date-pickers/index.ts
@@ -1,4 +1,2 @@
-export {DateTimePicker} from "./DateTimePicker";
-export {DatePicker} from "./DatePicker";
 export {PlainDatePicker} from "./PlainDatePicker";
 export {PlainDateTimePicker} from "./PlainDateTimePicker";
