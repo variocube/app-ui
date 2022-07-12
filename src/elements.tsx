48114f64edb1e19e435ce615a2d71e8fb479e7dd
Peter Karrer <peter@karrer.pro>
2022-07-12 13:20:43 +0200

    chore: remove deprecated SvgElement

--- src/elements.tsx
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-export const SvgElement = ({svg, className}: { svg: string, className?: string }) => {
-    return <span className={className} dangerouslySetInnerHTML={{ __html: svg }} />
-}
