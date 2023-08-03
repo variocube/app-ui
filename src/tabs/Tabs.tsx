605808c6ae970a80e4368c4f5921afcdcb6e3a39
Peter Karrer <peter@karrer.pro>
2023-08-03 13:22:15 +0200

    chore: simplify and improve selection behavior

--- src/tabs/Tabs.tsx
+++ src/tabs/Tabs.tsx
@@ -1,10 +1,5 @@
-import React, {
-    Fragment, SyntheticEvent,
-    useEffect,
-    useRef,
-    useState
-} from "react";
-import {Box, IconButton, ListItemIcon, ListItemText, Menu, MenuItem, Stack} from "@mui/material";
+import React, {Fragment, SyntheticEvent, useEffect, useRef, useState} from "react";
+import {IconButton, ListItemIcon, ListItemText, Menu, MenuItem, Stack} from "@mui/material";
 import MuiTabs, {TabsProps as MuiTabsProps} from "@mui/material/Tabs";
 import MoreHorizIcon from '@mui/icons-material/MoreHoriz';
 import MuiTab, {TabProps as MuiTabProps} from "@mui/material/Tab";
@@ -36,35 +31,40 @@ export function Tabs(props: TabsProps) {
         allowScrollButtonsMobile = true,
         sx,
         onChange,
-        items,
+        items: inItems,
+        value,
         ...rest
     } = props;
+
+    const items = inItems.map((item, i) => ({
+        ...item,
+        value: typeof item.value == "undefined" ? i : item.value
+    } as TabProps));
 
     const ref = useRef(null);
     const [dropdownEl, setDropdownEl] = useState<null|HTMLElement>(null);
 
-    const [tabItems, setTabItems] = useState<TabItem[]>([]);
-    const [hideIndexes, setHideIndexes] = useState<number[]|null>(null);
+    const [overflowIndex, setOverflowIndex] = useState<number>(items.length);
+
+    const selectedIndex = items.findIndex((item) => item.value == value);
 
     function handleTabOverflow(root: Element, items: TabItem[]) {
         // clientWidth: what the user can see
         // scrollWidth: actual size of the DOM object
         const {clientWidth} = root;
-        // console.log('detected tabs width changed', clientWidth);
 
         // this basically loop through each item size and adding up the width of all items
         // if the width exceed the clientWidth of the root node, item will be hidden from the horizontal tab display
         // else the item remains visible and its ref from the dropdown list will be hidden otherwise
-        const hideIndexes: number[] = [];
+        let overflowIndex = 0;
         let itemsWidth = 0;
         for (let i = 0; i < items.length; i++) {
             itemsWidth += items[i].width;
             if (itemsWidth < clientWidth) {
-                hideIndexes.push(i);
+                overflowIndex++;
             }
         }
-        setHideIndexes(hideIndexes);
-        // console.log('items hidden:', items.length - hideIndexes.length);
+        setOverflowIndex(overflowIndex);
     }
 
     useEffect(() => {
@@ -75,7 +75,6 @@ export function Tabs(props: TabsProps) {
                 // from the root node, traverse down to list node of tab items
                 const list = Array.from(root.children).find(e => e.className.includes('tabList'));
                 const items = list ? Array.from(list.children).map((e, index) => ({ index, width: e.clientWidth })) : [];
-                setTabItems(items);
 
                 // apply the resize observer to the root node and watch for the size changes
                 const resizeObserver = new ResizeObserver(() => handleTabOverflow(root, items));
@@ -90,6 +89,10 @@ export function Tabs(props: TabsProps) {
             flex={1}
             direction="row"
             alignItems="center"
+            sx={{
+                borderBottom: 1,
+                borderColor: "divider"
+            }}
         >
             <MuiTabs
                 ref={ref}
@@ -100,33 +103,37 @@ export function Tabs(props: TabsProps) {
                 sx={{
                     ...sx,
                     flex: 1,
-                    borderBottom: 1,
-                    borderColor: "divider"
                 }}
                 classes={{
                     scroller: 'tabRoot',
                     flexContainer: 'tabList'
                 }}
+                value={value}
                 onChange={onChange}
                 {...rest}
             >
                 {items
-                    .filter((tabProps, i) => !hideIndexes || hideIndexes.includes(i))
                     .map((tabProps, i) => (
                         <MuiTab
                             key={'muiTab-' + i}
                             iconPosition="start"
                             {...tabProps}
+                            sx={{
+                                ...tabProps.sx,
+                                ...(i >= overflowIndex ? {display: "none"} : undefined),
+                                minHeight: 48,
+                            }}
                         />
                     ))}
             </MuiTabs>
-            {(hideIndexes && hideIndexes.length < items.length) && (
+            {(overflowIndex < items.length) && (
                 <Fragment>
                     <IconButton
                         sx={{
                             ml: 1
                         }}
                         onClick={ev => setDropdownEl(ev.currentTarget)}
+                        color={selectedIndex >= overflowIndex ? "primary" : "default"}
                     >
                         <MoreHorizIcon fontSize="inherit" />
                     </IconButton>
@@ -142,21 +149,29 @@ export function Tabs(props: TabsProps) {
                                     icon,
                                     onClick,
                                     component,
+                                    value: itemValue,
                                     ...rest
                                 } = tabProps;
-                                let handleClick = onClick;
-                                if (!component && onChange) {
-                                    handleClick = (ev: SyntheticEvent) => {
-                                        console.log('change', i);
-                                        onChange(ev, i)
-                                    };
+
+                                const selected = value == itemValue;
+
+                                function handleClick(ev: SyntheticEvent) {
+                                    if (!selected && onChange) {
+                                        onChange(ev, i);
+                                    }
+                                    if (onClick) {
+                                        onClick(ev);
+                                    }
+                                    setDropdownEl(null);
                                 }
+
                                 return (
                                     <MenuItem
                                         key={'dropdownItem-' + i}
                                         onClick={handleClick}
                                         component={component}
-                                        sx={(hideIndexes && !hideIndexes.includes(i)) ? {} : hiddenSx}
+                                        sx={i < overflowIndex ? hiddenSx : {}}
+                                        selected={selected}
                                         {...rest}
                                     >
                                         {icon && <ListItemIcon>{icon}</ListItemIcon>}
