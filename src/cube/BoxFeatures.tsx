44002bba39f99092143133ff9f26e7ffaadd339f
Grigor <grigor.tonkov@gmail.com>
2024-01-08 16:40:00 +0100

    make translatable box features

--- src/cube/BoxFeatures.tsx
+++ src/cube/BoxFeatures.tsx
@@ -2,18 +2,22 @@ import {SvgIconComponent} from "@mui/icons-material";
 import AccessibleIcon from "@mui/icons-material/Accessible";
 import AcUnitIcon from "@mui/icons-material/AcUnit";
 import WarningAmberIcon from '@mui/icons-material/WarningAmber';
+import ChargerIcon from '@mui/icons-material/ChargingStation';
 import {Chip, Tooltip, useTheme} from "@mui/material";
 import {blue, lightBlue, red, yellow} from "@mui/material/colors";
-import React, {Fragment} from "react";
+import React, {Fragment} from "react"; 
 
-export function BoxFeatures({box: { accessible, cooled, dangerousGoods }, minimized, renderEmpty}: { box: Box, minimized?: boolean, renderEmpty?: boolean }) {
+export interface BoxFeaturesProps { box: Box, minimized?: boolean, renderEmpty?: boolean, t: Function }
+
+export function BoxFeatures({box: { accessible, cooled, dangerousGoods, charger }, minimized, renderEmpty, t}: BoxFeaturesProps) {
 	if (renderEmpty && !accessible && !cooled && !dangerousGoods) return <span>None</span>;
-	const flagProps = { minimized };
+	const flagProps = { minimized, t };
 	return (
 		<Fragment>
 			{accessible && <Accessible {...flagProps} />}
 			{cooled && <AcUnit {...flagProps} />}
 			{dangerousGoods && <DangerousGood {...flagProps} />}
+			{charger && <Charger {...flagProps} />}
 		</Fragment>
 	);
 }
@@ -32,25 +36,35 @@ export interface Box {
 	accessible: boolean;
 	cooled: boolean;
 	dangerousGoods: boolean;
+	charger: boolean;
 }
 
 
 interface FlagProps {
 	minimized?: boolean;
+	t?:Function
 }
 
-function Accessible(props: FlagProps) {
+function Accessible(props: FlagProps) { 
 	const theme = useTheme();
 	const color = theme.palette.mode == "dark" ? yellow["400"] : blue["700"];
-	return <FlagChip label="Accessible" color={color} Icon={AccessibleIcon} {...props} />;
+	const label = props.t?props.t('BoxType.Accessible'):'Accessible';
+	return <FlagChip label={label} color={color} Icon={AccessibleIcon} {...props} />;
 }
 
 function AcUnit(props: FlagProps) {
-	return <FlagChip label="Cooled" color={lightBlue["400"]} Icon={AcUnitIcon} {...props} />;
+	const label = props.t?props.t('BoxType.Cooled'):'Cooled';
+	return <FlagChip label={label} color={lightBlue["400"]} Icon={AcUnitIcon} {...props} />;
 }
 
 function DangerousGood(props: FlagProps) {
-	return <FlagChip label="Dangerous Goods" color={red['400']} Icon={WarningAmberIcon} {...props} />;
+	const label = props.t?props.t('BoxType.Dangerous Goods'):'Dangerous Goods';
+	return <FlagChip label={label} color={red['400']} Icon={WarningAmberIcon} {...props} />;
+}
+
+function Charger(props: FlagProps) {
+	const label = props.t?props.t('BoxType.Charger'):'Charger';
+	return <FlagChip label={label} color={red['400']} Icon={ChargerIcon} {...props} />;
 }
 
 interface FlagChipProps extends FlagProps {
