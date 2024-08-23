126f08696902e6ead6ce3c79b71965776f914d12
banart <dev@banart.tech>
2024-08-23 06:51:04 +0200

    fix: bug in useStorage

--- src/storage/useStorage.ts
+++ src/storage/useStorage.ts
@@ -2,31 +2,34 @@ import {useCallback, useEffect, useMemo, useState} from "react";
 import {storage} from "./storage";
 
 export function useStorage<T>(key: string, defaultValue: T): [T, (newValue: T) => void] {
+	const defaultValueSerialized = useMemo(() => JSON.stringify(defaultValue), [defaultValue]);
 
-    const defaultValueSerialized = useMemo(() => JSON.stringify(defaultValue), [defaultValue]);
+	const readStateFromStorage = useCallback(() => {
+		const storageValue = storage.read(key);
+		return storageValue ?? defaultValueSerialized;
+	}, [key, defaultValueSerialized]);
 
-    const readStateFromStorage = useCallback(() => {
-        const storageValue = storage.read(key);
-        return storageValue ?? defaultValueSerialized;
-    }, [key, defaultValueSerialized]);
+	const [value, setValue] = useState(readStateFromStorage);
 
-    const [value, setValue] = useState(readStateFromStorage);
+	const updateStateFromStorage = useCallback(() => {
+		setValue(readStateFromStorage());
+	}, [readStateFromStorage]);
 
-    const updateStateFromStorage = useCallback(() => {
-        setValue(readStateFromStorage());
-    }, [readStateFromStorage]);
+	useEffect(() => {
+		storage.addChangeListener(key, updateStateFromStorage);
+		return () => storage.removeChangeListener(key, updateStateFromStorage);
+	}, [key, updateStateFromStorage]);
 
-    useEffect(() => {
-        storage.addChangeListener(key, updateStateFromStorage);
-        return () => storage.removeChangeListener(key, updateStateFromStorage);
-    }, [key, updateStateFromStorage]);
+	useEffect(() => {
+		storage.write(key, value == defaultValueSerialized ? undefined : value);
+	}, [key, value, defaultValueSerialized]);
 
-    useEffect(() => {
-        storage.write(key, value == defaultValueSerialized ? undefined : value);
-    }, [key, value, defaultValueSerialized]);
+	const typedValue = useMemo(() => JSON.parse(value), [value]);
 
-    const typedValue = useMemo(() => JSON.parse(value), [value]);
-    const setTypedValue = useCallback((newValue: T) => setValue(JSON.stringify(newValue)), []);
+	const setTypedValue = useCallback((newValue: T) => {
+		const value = JSON.stringify(newValue);
+		storage.write(key, value == defaultValueSerialized ? undefined : value);
+	}, [key, defaultValueSerialized]);
 
-    return [typedValue, setTypedValue];
+	return [typedValue, setTypedValue];
 }
