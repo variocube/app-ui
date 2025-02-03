bd86080d3a0ad6b5082c8c20fa74b6718de89166
banartdev <office@banart.tech>
2025-02-03 06:22:00 +0100

    feat: support for py on View which defaults to 4 feat: allow to pass ContainerProps to View

--- src/view/View.tsx
+++ src/view/View.tsx
@@ -2,18 +2,18 @@ import {Container, ContainerProps, Stack} from "@mui/material";
 import React, {PropsWithChildren} from "react";
 import {ErrorAlert} from "../ErrorAlert";
 
-export interface ViewProps {
-	maxWidth?: ContainerProps["maxWidth"];
+export interface ViewProps extends ContainerProps {
 	loading?: boolean;
 	error?: any;
 	spacing?: number;
+	py?: number;
 }
 
 export function View(props: PropsWithChildren<ViewProps>) {
-	const {maxWidth, loading, error, spacing = 2, children} = props;
+	const {maxWidth, loading, error, spacing = 2, py = 4, children, ...containerProps} = props;
 	return (
-		<Container maxWidth={maxWidth}>
-			<Stack spacing={spacing} sx={{opacity: loading ? 0.5 : 1}}>
+		<Container maxWidth={maxWidth} {...containerProps}>
+			<Stack spacing={spacing} py={py} sx={{opacity: loading ? 0.5 : 1}}>
 				{error && <ErrorAlert error={error} />}
 				{children}
 			</Stack>
